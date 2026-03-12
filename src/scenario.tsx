80914217901ca62e0ac448c2f6c3f381f5848594
Maksim Grushin <grusha-as@yandex.ru>
2026-03-12 15:13:37 +0500

    Adding screewriter project files Version 0.1.0

---
+++ src/scenario.tsx
@@ -0,0 +1,2 @@
+import 'scenario.css';
+
---
+++ src/templates/panel.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import 'panel.css';
+
+function initPanel(type: string) {
+    const = 
+}
+
+function Panel() {
+    const [list, setList] = useState();
+
+    return(
+        <>
+
+        </>
+    )
+}
