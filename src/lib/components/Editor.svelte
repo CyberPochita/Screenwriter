7abd4581e2f256e2f9fa1ae4dccc7e344f5eed6c
Maksim Grushin <maksimgrusin963@gmail.com>
2026-05-26 16:58:54 +0500

    Changed the design of the editor

--- src/lib/components/Editor.svelte
+++ src/lib/components/Editor.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import CodeMirror from "svelte-codemirror-editor";
-  import { markdown } from "@codemirror/lang-markdown";
   
   // Используем $bindable для связи с родителем
   let { 
@@ -9,29 +8,15 @@
   } = $props();
 </script>
 
-<div class="editor-wrapper h-full flex-1 overflow-auto font-mono text-lg">
+<div class="editor-wrapper h-full flex-1 overflow-auto font-mono text-lg bg-white/90 rounded-lg p-4 shadow-inner">
   <CodeMirror
     bind:value={value}
     onready={(v) => (view = v)}
-    lang={markdown()}
     rectangularSelection={false}
     lineWrapping={true}
-    styles={{
-      "&": { height: "100%", backgroundColor: "transparent" },
-      "&.cm-focused": { outline: "none" },
-      ".cm-scroller": { fontFamily: "var(--font-mono)", lineHeight: "1.8" },
-      ".cm-content": { padding: "10px" },
-      ".cm-gutters": { 
-        backgroundColor: "transparent", 
-        borderRight: "1px solid rgba(25, 25, 25, 0.2)", 
-        color: "rgba(25, 25, 25, 0.5)", 
-        borderLeft: "none" 
-      },
-      ".cm-activeLineGutter": { 
-        backgroundColor: "transparent", 
-        color: "var(--color-writer-focus)" 
-      },
-    }}
+    lineNumbers={false}
+    foldGutter={false}
+    highlight={false as any}
   />
 </div>
 
